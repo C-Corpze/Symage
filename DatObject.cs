8f06099fd4dd5dc3855810e89b531eb1b76305ed
Maxym <rubyepicfox@gmail.com>
2025-06-16 15:40:17 +0200

    I buncha testing.

---
+++ DatObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+namespace Symage
+{
+    public class DatObject
+    {
+
+        List<byte> Bytes = new List<byte>();
+
+
+        void clearByteList()
+        {
+            Bytes.Clear();
+        }
+
+
+        void addByte(byte num)
+        {
+            Bytes.Add(num);
+        }
+
+
+        void add16Bit(ushort num)
+        {
+
+        }
+
+        
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+namespace Symage;
+
+
+
+
+internal class Program
+{
+
+
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hit enter to start.");
+
+        //var input = Console.ReadLine();
+
+        //Console.WriteLine($"You entered: {input}");
+
+
+        int number = 1234567890;
+
+        byte b1 = (byte)(number & 0xff);
+
+        byte b2 = (byte)((number >> 8) & 0xff);
+
+        byte b3 = (byte)((number >> 16) & 0xff);
+
+        byte b4 = (byte)((number >> 24) & 0xff);
+
+
+        Console.WriteLine($"The byte representation of {number} is: {b1} - {b2} - {b3} - {b4}");
+
+
+    }
+
+
+
+}
