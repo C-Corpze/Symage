8b0606376be137c091944a22b27b914675a244e1
Maxym <rubyepicfox@gmail.com>
2025-06-19 12:26:26 +0200

    Added testing code.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using ImageMagick;
+using NAudio;
+
 namespace Symage;
 
 internal class Program
@@ -7,25 +10,12 @@ internal class Program
 	static void Main(string[] args)
 	{
 		//Console.WriteLine("Hit enter to start.");
-
 		//var input = Console.ReadLine();
-
 		//Console.WriteLine($"You entered: {input}");
 
+		Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
 
-		int number = 1234567890;
-
-		byte b1 = (byte)(number & 0xff);
-
-		byte b2 = (byte)((number >> 8) & 0xff);
-
-		byte b3 = (byte)((number >> 16) & 0xff);
-
-		byte b4 = (byte)((number >> 24) & 0xff);
-
-
-		Console.WriteLine($"The byte representation of {number} is: {b1} - {b2} - {b3} - {b4}");
-
+		var img = new MagickImage(AppDomain.CurrentDomain.BaseDirectory + "testimg.webp");
 
 	}
 
