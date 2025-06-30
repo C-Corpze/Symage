c5a1bffaf784cb8cf1b019be7e9092d94abf6d8b
Maxym <rubyepicfox@gmail.com>
2025-06-30 15:24:58 +0200

    Whole lotta edits.

--- DatObject.cs
+++ DatObject.cs
@@ -11,42 +11,71 @@ using System.Threading.Tasks;
 
 namespace Symage
 {
+	// Wahoo, the data object.
 	public class DatObject
 	{
 
-		List<byte> bytelist = new List<byte>();
+		//List<byte> bytelist = new List<byte>();
+
+		byte[] byte_array;
+
+		int index = 0;
 
 
-		void clearByteList() { bytelist.Clear(); }
 
-		// Adds a single byte to the list.
-		void addByte(byte num) { bytelist.Add(num); }
-
-		// Splits a 16-bit integer into 2 bytes and adds them to the list.
-		void add16Bit(ushort num)
+		public DatObject(int asize = 10)
 		{
-			bytelist.Add(BitConv.get_byte(num, 1)); // Add the first byte.
-			bytelist.Add(BitConv.get_byte(num, 0)); // Add the second byte.
+			// Constructor, initializes the byte array with a default size of 5.
+			byte_array = new byte[asize];
 		}
 
-		// Splits an integer into 3 bytes and adds them to the list.
-		// C# however doesn't really have 24-bit numbers so it just discards the 4th byte.
-		void add24Bit(int num)
+
+		// This just initializes an byte array, you must know the size beforehand.
+		public void initArray(int asize)
 		{
-			bytelist.Add(BitConv.get_byte(num, 2)); // Add the first byte.
-			bytelist.Add(BitConv.get_byte(num, 1)); // Add the second byte.
-			bytelist.Add(BitConv.get_byte(num, 0)); // Add the third byte.
-													// The fourth byte is discarded.
+			byte_array = new byte[asize];
+		}
+
+
+		// This will add one (1) byte into the array, or rather overwrites one.
+		public void addByte(byte b)
+		{
+			// Dead-simple measure to prevent ever going outta bounds.
+			if ( index > byte_array.Length ) { index = 0; }
+
+			byte_array[index] = b; // Add one byte at a time and move the index.
+			index++;
+		}
+
+		// Splits an 16-bit integer into 2 bytes and adds them to the array.
+		public void add16Bit(short num)
+		{
+			addByte(BitConv.get_byte(num, 1)); // Add the first byte.
+			addByte(BitConv.get_byte(num, 0)); // Add the second byte.
 		}
 
 		// Splits an integer into 4 bytes and adds them to the list.
-		void add32Bit(int num)
+		public void add32Bit(int num)
 		{
-			bytelist.Add(BitConv.get_byte(num, 3)); // Add the first byte.
-			bytelist.Add(BitConv.get_byte(num, 2)); // Add the second byte.
-			bytelist.Add(BitConv.get_byte(num, 1)); // Add the third byte.
-			bytelist.Add(BitConv.get_byte(num, 0)); // Add the fourth byte.
+			addByte(BitConv.get_byte(num, 3)); // Add the first byte.
+			addByte(BitConv.get_byte(num, 2)); // Add the second byte.
+			addByte(BitConv.get_byte(num, 1)); // Add the third byte.
+			addByte(BitConv.get_byte(num, 0)); // Add the fourth byte.
 		}
+
+
+		// Splits an integer into 3 bytes and adds them to the list.
+		// C# however doesn't really have 24-bit numbers so it just discards the 4th byte.
+		// (Dunno if I'll use this.)
+		public void add24Bit(int num)
+		{
+			addByte(BitConv.get_byte(num, 2)); // Add the first byte.
+			addByte(BitConv.get_byte(num, 1)); // Add the second byte.
+			addByte(BitConv.get_byte(num, 0)); // Add the third byte.
+												// The fourth byte is yeeted lol.
+		}
+
+		
 
 	}
 }
--- Program.cs
+++ Program.cs
@@ -16,28 +16,28 @@ internal class Program
 		Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
 
 
-		MagickImage img = new MagickImage("D:\\_ASSETS\\_MyPrograms\\Symage\\Symage\\testimg.webp");
+		MagickImage image = new MagickImage("D:\\_ASSETS\\_MyPrograms\\Symage\\Symage\\testimg.webp");
 
-		Console.WriteLine($"Image width: {img.Width}, height: {img.Height}");
+		Console.WriteLine($"Image width: {image.Width}, height: {image.Height}");
 
 
-		IPixelCollection<byte> pixels = img.GetPixels();
+		IPixelCollection<byte> pixels = image.GetPixels();
 
 		// Iterate through pixel collection.
-		for ( int x = 0; x < img.Width; x++ )
+		for ( int x = 0; x < image.Width; x++ )
 		{
-			for ( int y = 0; y < img.Height; y++ )
+			for ( int y = 0; y < image.Height; y++ )
 			{
 				IPixel<byte> pixel = pixels.GetPixel(x, y);
 
-				int pix_to_num = BitConv.make_int32(
+				int rgba_int = BitConv.make_int32(
 					pixel.GetChannel(0),
 					pixel.GetChannel(1),
 					pixel.GetChannel(2),
 					pixel.GetChannel(3)
-					);
+				);
 
-				Console.WriteLine($"Pixel ({x}, {y}): {pix_to_num:x8}");
+				Console.WriteLine($"Pixel ({x}, {y}): {rgba_int:x8}");
 
 				//Console.WriteLine($"Pixel ({x}, {y}): R={pixel.GetChannel(0)}, G={pixel.GetChannel(1)}, B={pixel.GetChannel(2)}, A={pixel.GetChannel(3)}");
 
