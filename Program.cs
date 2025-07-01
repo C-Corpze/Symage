8b550ab1f944c28f938d206a941285524caad129
Maxym <rubyepicfox@gmail.com>
2025-07-01 15:49:01 +0200

    First functional test.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 using ImageMagick;
 using NAudio;
+using NAudio.Wave;
 
 namespace Symage;
 
@@ -21,8 +22,15 @@ internal class Program
 		MagickImage image = new MagickImage("D:\\_ASSETS\\_MyPrograms\\Symage\\Symage\\testimg.webp");
 		Console.WriteLine($"Image width: {image.Width}, height: {image.Height}");
 
+		DatObject dat_object = ImgClass.decodeImage32Bit(image);
 
-	
+
+		WaveFileWriter wave_writer = new WaveFileWriter(
+			"D:\\_ASSETS\\_MyPrograms\\Symage\\Symage\\test.wav",
+			new WaveFormat(44100, 32, 1)
+		);
+
+		wave_writer.Write(dat_object.byte_array, 0, dat_object.byte_array.Length);
 
 
 	}
