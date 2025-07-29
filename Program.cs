d7e5b89b242b40a47e90adb11de5b83558220c5f
Maxym <rubyepicfox@gmail.com>
2025-07-29 12:31:30 +0200

    Working on the input.

--- Program.cs
+++ Program.cs
@@ -17,28 +17,56 @@ public static class Program
 		FileMan.getDirInApp( "_audio" );
 		FileMan.getDirInApp( "_output" );
 
-		Console.WriteLine( $"App is settled in {FileMan.base_dir}.\n" );
-		Console.WriteLine( "Symage will automatically batch-process images and sounds placed in\ntheir designated folders.\n" );
-
-		Console.WriteLine( $"Images go in    >  {FileMan.getDirInApp( "_images" )}." );
-		Console.WriteLine( $"Audio goes in   >  {FileMan.getDirInApp( "_audio" )}." );
-		Console.WriteLine( $"Output goes to  >  {FileMan.getDirInApp( "_output" )}." );
-
-
-
-		while (true)
+		while ( true )
 		{
+			Console.WriteLine( $"\nApp is settled in {FileMan.base_dir}.\n" );
+			Console.WriteLine( "Symage will automatically batch-process images and sounds placed in \ntheir designated folders.\n" );
+
+			Console.WriteLine( $"Images go in    >  {FileMan.getDirInApp( "_images" )}." );
+			Console.WriteLine( $"Audio goes in   >  {FileMan.getDirInApp( "_audio" )}." );
+			Console.WriteLine( $"Output goes to  >  {FileMan.getDirInApp( "_output" )}." );
+
+
+
+
 			Console.WriteLine( "\nHit enter to start Symage.\n" );
 			string? input = Console.ReadLine();
 
 
-			Console.WriteLine("Select mode:\n\n");
+			Console.WriteLine( "Select mode, type and press enter to select:\n\n" );
 			Console.WriteLine( "i - Convert images to WAV files.\n" );
 			Console.WriteLine( "a - Convert audio to PNG files.\n" );
 
 
 
+			input = Console.ReadLine();
+			Console.Clear();
 
+			switch ( input )
+			{
+				case "i":
+				{
+					Console.WriteLine( "\nYou chose converting images to audio.\n" );
+					Console.WriteLine( $"Place image files (PNG, JPG, WEBP) in    >  {FileMan.getDirInApp( "_images" )}.\n" );
+					convertImageToAudio();
+					break;
+				}
+
+				case "a":
+				{
+					Console.WriteLine( "\nYou chose converting audio to images.\n" );
+					Console.WriteLine( $"Place audio files (WAV, MP3, OGG) in    >  {FileMan.getDirInApp( "_audio" )}.\n" );
+					//convertImageToAudio();
+					break;
+				}
+
+				default:
+				{
+					Console.WriteLine( "\nNothing was selected, please try again.\n" );
+					break;
+				}
+
+			}
 		}
 
 
@@ -60,6 +88,25 @@ public static class Program
 
 		Image24.encodeBytesRGB( debug_dir + "audio_to_image.png", aud_dat, 256 * 2 );
 
+	}
+
+
+	public static void convertImageToAudio()
+	{
+		Console.WriteLine( "\nEnter your desired sample rate. \nExample sample rates are:" );
+
+		Console.WriteLine( "\n- 44100 - Typical for CD & DVD audio." );
+		Console.WriteLine( "\n- 48000 - Used for studio recordings & professional sound editing." );
+		Console.WriteLine( "\n- 32000 - Has a maximum (nyquist) frequency of 16khz (basically MP3 quality), \nlonger and lower-pitched WAV file." );
+
+		int sample_rate = UserInput.getIntFromUser( "\nEnter target sample rate (default 44100): ", 44100 );
+		int channels = UserInput.getIntFromUser( "Enter number of channels (default 2): ", 2 );
+	}
+
+	public static void convertAudioToImage()
+	{
+		int res_x = UserInput.getIntFromUser( "Enter the X resolution of the image (default 512): ", 512 );
+		int res_y = UserInput.getIntFromUser( "Enter the Y resolution of the image (default 512): ", 512 );
 	}
 
 
--- UserInput.cs
+++ UserInput.cs
@@ -1,100 +1,39 @@
 ﻿namespace Symage
 {
-	public class UserInput
+	public static class UserInput
 	{
 
 		// Primitive functions for reading input.
 
 
-		public int getIntFromUser( int default_value = 0 )
+		public static int getIntFromUser( string text_message, int default_value = 0 )
 		{
+			Console.WriteLine( text_message );
+
 			string? text = Console.ReadLine();
 			if ( text == null ) return default_value;
 
 			try { return Int32.Parse( text ); }
 			catch
 			{
-				Console.WriteLine( $"Invalid input, defaulting to {default_value}." );
+				Console.WriteLine( $"\nInvalid or no input, defaulting to {default_value}." );
 				return default_value;
 			}
 		}
 
-		public float getFloatFromUser( float default_value = 0.0f )
+		public static float getFloatFromUser( string text_message, float default_value = 0.0f )
 		{
+			Console.WriteLine( text_message );
+
 			string? text = Console.ReadLine();
 			if ( text == null ) return default_value;
 
 			try { return float.Parse( text ); }
 			catch
 			{
-				Console.WriteLine( $"Invalid input, defaulting to {default_value}." );
+				Console.WriteLine( $"\nInvalid or no input, defaulting to {default_value}." );
 				return default_value;
 			}
-		}
-
-
-
-
-
-
-		// The sound part.
-
-		public ushort sound_decode_mode = 0;
-		public string[] sound_decode_modes = [
-			"32-bit Decode (Any format)",
-			"16-bit Decode (WAV only)"
-		];
-
-		public ushort sound_encode_mode = 0;
-		public string[] sound_encode_modes = [
-			"32-bit Float Encode",
-			"16-bit Encode (WAV only)"
-		];
-
-
-		// The image part.
-
-		public ushort image_decode_mode = 0;
-		public string[] image_decode_modes = [
-			"24-bit BitClap (Any format)",
-			"32-bit BitClap (RGBA) (PNG & WEBP Only)"
-		];
-
-		public ushort image_encode_mode = 0;
-		public string[] image_encode_modes = [
-			"24-bit BitCircumcision",
-			"32-bit BitCircumcision (RGBA) (PNG & WEBP Only)",
-		];
-
-
-		public ushort selectChoice( string[] choices )
-		{
-			Console.WriteLine( "Press a number followed by the [Enter] key to select a mode." );
-
-			for ( int i = 0; i < choices.Length; i++ )
-			{
-				Console.WriteLine( $"Mode: {i} = {choices[ i ]}" );
-			}
-
-
-			string? input = Console.ReadLine();
-
-			if ( input == null )
-			{
-				Console.WriteLine( "No input received, defaulting to 0." );
-				return 0; // Default to the first mode if no input is received.
-			}
-
-
-			ushort choice = 0;
-
-			if ( ushort.TryParse( input, out ushort num ) )
-			{
-				choice = num;
-			}
-
-			Console.WriteLine( $"{choice} was chosen." );
-			return choice;
 		}
 
 
