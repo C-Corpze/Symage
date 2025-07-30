d11d671ce393f0e2ab83eaeb94f5de11a2e32348
Maxym <rubyepicfox@gmail.com>
2025-07-30 14:31:10 +0200

    Backup.

--- Program.cs
+++ Program.cs
@@ -72,7 +72,7 @@ public static class Program
 
 		// Some testing code.
 
-		Console.WriteLine( $"Encoding image nto audio." );
+		Console.WriteLine( $"Encoding image into audio." );
 		MagickImage image = new MagickImage( debug_dir + "testimg.webp" );
 
 		SampleDataObject dat = Image24.decodeBytesRGB( image );
@@ -99,14 +99,14 @@ public static class Program
 		Console.WriteLine( "\n- 48000 - Used for studio recordings & professional sound editing." );
 		Console.WriteLine( "\n- 32000 - Has a maximum (nyquist) frequency of 16khz (basically MP3 quality), \nlonger and lower-pitched WAV file." );
 
-		int sample_rate = UserInput.getIntFromUser( "\nEnter target sample rate (default 44100): ", 44100 );
-		int channels = UserInput.getIntFromUser( "Enter number of channels (default 2): ", 2 );
+		int sample_rate = UserInput.getIntFromUser( "\nEnter target sample rate (default: 44100): ", 44100 );
+		int channels = UserInput.getIntFromUser( "Enter number of channels (default: 2 (stereo)): ", 2 );
 	}
 
 	public static void convertAudioToImage()
 	{
-		int res_x = UserInput.getIntFromUser( "Enter the X resolution of the image (default 512): ", 512 );
-		int res_y = UserInput.getIntFromUser( "Enter the Y resolution of the image (default 512): ", 512 );
+		int res_x = UserInput.getIntFromUser( "Enter the X resolution of the image (default: 512): ", 512 );
+		int res_y = UserInput.getIntFromUser( "Enter the Y resolution of the image (default: auto): ", 0 );
 	}
 
 
